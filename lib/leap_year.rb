bbc62b858c0c4427d06b8b131a6fcda969c7b701
Jenny Allar <JennyAllar@gmail.com>
2014-03-11 15:07:31 -0600

    Determines if a year is divisible by both 400 & 100
    Changed initial test (divisible by 4) to pending to single out second test.

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -4,8 +4,14 @@ class Year
   end
 
   def yes?
-    if @leap % 4 == 0
-      "Yay! Leap year!"
+    #if @leap % 4 == 0
+     # "Yay! Leap year!"
+    #end
+
+    if (@leap % 400 == 0) && (@leap % 100 == 0)
+      return "Yay! Leap year!"
+    else
+      return "Boo, no leap year!"
     end
 
 
--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -2,7 +2,16 @@ require 'leap_year'
 
 describe "leap_year" do
   it "determines if a year is divisible by 4" do
-  chosen_year = Year.new(1992)
+    pending
+  chosen_year = Year.new(4000)
+  expected = "Yay! Leap year!"
+  actual = chosen_year.yes?
+
+  expect(actual).to eq expected
+  end
+
+  it "determines if a year is divisible by both 100 & 400" do
+  chosen_year = Year.new(2000)
   expected = "Yay! Leap year!"
   actual = chosen_year.yes?
 
