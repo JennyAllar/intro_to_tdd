35166ac563df290d1afab80b4e0362e44e184aad
Jenny Allar <JennyAllar@gmail.com>
2014-03-15 13:43:03 -0600

    Added subtract feature and save/get feature.

--- lib/numeric_calculator.rb
+++ lib/numeric_calculator.rb
@@ -2,4 +2,16 @@ class NumericCalculator
   def add(x, y)
     total = x + y
   end
+
+  def subtract(x, y)
+    total = x - y
+  end
+
+  def save(z)
+    @saved_value = z
+  end
+
+  def get
+    @saved_value
+  end
 end
--- spec/numeric_calculator_spec.rb
+++ spec/numeric_calculator_spec.rb
@@ -10,7 +10,25 @@ describe NumericCalculator do
 
     expect(result).to eq 11
 
-
   end
+
+
+  it "subtracts two numbers" do
+    calculator = NumericCalculator.new
+
+    result = calculator.subtract(9, 7)
+
+    expect(result).to eq 2
+  end
+
+  it "saves a number to the memory" do
+    calculator = NumericCalculator.new
+
+    calculator.save(123)
+    result = calculator.get
+
+    expect(result).to eq 123
+  end
+
 
 end
